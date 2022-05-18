d49d45f5c8e1b8433666192a11605d370fd568f3
Luiz Paulo <62365747+luizpaulo2005@users.noreply.github.com>
2022-05-18 21:13:09 +0000

    Initial commit

---
+++ migrations/20220518211114-create-livro.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('livros', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      editora: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      data_publicacao: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      preco: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('livros');
+  }
+};
